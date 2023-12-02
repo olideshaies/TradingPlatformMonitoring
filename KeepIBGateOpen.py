c668939ee3c5082e5ed3458373cd89797679a5bc
olideshaies <deshaiesg@hotmail.com>
2023-12-02 15:29:37 -0500

    adding flexibility to chosse which IB app is used TWS or Gateway

--- KeepIBGateOpen.py
+++ KeepIBGateOpen.py
@@ -4,9 +4,11 @@ import time
 import pyautogui
 from cryptography.fernet import Fernet
 
-def is_gateway_running():
-    # Process name to look for (adjust if needed based on the exact name from Task Manager)
-    target_process_name = "ibgateway.exe"
+def is_IB_app_running(running_app):
+    if running_app == "TWS":
+        target_process_name = "tws.exe"
+    elif running_app == "Gateway":
+        target_process_name = "ibgateway.exe"
     
     for process in psutil.process_iter():
         try:
@@ -16,8 +18,12 @@ def is_gateway_running():
             pass
     return False
 
-def start_gateway():
-    ib_gateway_path = r"C:\Jts\ibgateway\1024\ibgateway.exe"
+def start_IB_app(running_app):
+    # Need to add the option to open based on the IB app used (TWS or Gateway)
+    if running_app == "TWS":
+        ib_gateway_path = r"C:\Jts\tws.exe"
+    elif running_app == "Gateway":
+        ib_gateway_path = r"C:\Jts\ibgateway\1024\ibgateway.exe"
     subprocess.Popen(ib_gateway_path)  # Removed the argument since it's probably unnecessary
 
 def load_key():
@@ -27,13 +33,19 @@ def decrypt_message(encrypted_message, key):
     f = Fernet(key)
     return f.decrypt(encrypted_message).decode()
 
-def login_to_gateway():
-    coordinate_x = 954
-    coordinate_y = 1909
+def login_to_IB_app(running_app):
+    if running_app == "TWS":
+        # Coordinates for the Paper Trading
+        coordinate_x = 801
+        coordinate_y = 454
+    elif running_app == "Gateway":
+        # Coordinates for the Paper Trading
+        coordinate_x = 801
+        coordinate_y = 454
 
     pyautogui.click(x=coordinate_x, y=coordinate_y)
     
-    time.sleep(2)
+    time.sleep(1)
     
     pyautogui.write(username)
     pyautogui.press('tab')  
@@ -51,17 +63,22 @@ key = load_key()
 username = decrypt_message(encrypted_username, key)
 password = decrypt_message(encrypted_password, key)
 
+running_app = input("Which IB app are you using? (TWS or Gateway): ")
+
 # Main loop
 while True:
-    if not is_gateway_running():
+    if not is_IB_app_running(running_app):
         print("IBKR Gateway is not running. Starting it...")
-        start_gateway()
+        start_IB_app(running_app)
         
         # Add a bit more wait time for the app to fully launch
-        time.sleep(10)
-        login_to_gateway()
+        if running_app == "TWS":
+            time.sleep(5)
+        elif running_app == "Gateway":
+            time.sleep(3)
+        login_to_IB_app(running_app)
     else:
-        print("IBKR Gateway is running.")
+        print("IB App is running.")
     
     # Check every minute
-    time.sleep(10)
+    time.sleep(3)
